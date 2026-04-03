8348743a40df1e0dc87ace8dcc817d549ce97847
Vaibhav <vaibhavkapoor0192@gmail.com>
2026-04-04 01:57:50 +0530

    Update page.tsx

--- Client/src/components/pages/ProjectsPage/page.tsx
+++ Client/src/components/pages/ProjectsPage/page.tsx
@@ -1,6 +1,6 @@
 // app/projects/page.tsx
 import { useEffect, useState } from "react";
-import ProjectsList from "./projectList";
+import ProjectsList from "./ProjectList";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import NotFoundPage from "../notFoundPage/page";
